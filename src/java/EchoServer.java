323bc3b04f4782bc3baf504f8a2500ea69bf5d7e
MagnusArias <magnusarias.95@gmail.com>
2017-06-08 22:08:36 +0200

    Clean code
    Zmiany w przejrzystości kodu, naprawione wcięcia, zbędne wywołania funkcji pousuwane

--- src/java/EchoServer.java
+++ src/java/EchoServer.java
@@ -10,33 +10,39 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
  
 @ServerEndpoint("/echo") 
-public class EchoServer {
+public class EchoServer
+{
     
     private static final int MAX_PLAYERS = 2;
+    private static Random xPosGen = new Random();
+    private static Random yPosGen = new Random();
+    private static List<Session> players = new ArrayList<>();
+    private int playersID = 0;
     
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session) 
+    {
         System.out.println(session.getId() + " has opened a connection");
-        try {
+        try 
+        {
             SessionHandler.addSession(session);
-            
             session.getBasicRemote().sendText("Connection Established"); 
-        } catch (IOException ex) {
+        } catch (IOException ex) 
+        {
             ex.printStackTrace();
         }
     }
 
     @OnMessage
-    public void onMessage(String message, Session session) {
+    public void onMessage(String message, Session session) 
+    {
         handle(message, session);
-
     }
-    private static Random xPosGen = new Random();
-    private static Random yPosGen = new Random();
-    private static List<Session> players = new ArrayList<>();
-    private int playersID = 0;
-    private void handle(String csv, Session session) {
+   
+    private void handle(String csv, Session session) 
+    {
         String[] protocol = csv.split(":");
+        
         /* protocol:
             [0] = MainProtocol
             [1] = player
@@ -47,68 +53,74 @@ public class EchoServer {
         switch(protocol[0]) { 
                             
             case "0":   //start - oczekiwanie
-                    int newX = xPosGen.nextInt(800);
-                    int newY = yPosGen.nextInt(600);  
-                    System.out.println("player got ID: " + playersID);
-                    session.getUserProperties().put("playerID", playersID); //zapisuje polozenie  gracza
-                    session.getUserProperties().put("xPos", newX); //zapisuje polozenie gracza
-                    session.getUserProperties().put("yPos", newY); //zapisuje polozenie gracza
+                int newX = xPosGen.nextInt(800);
+                int newY = yPosGen.nextInt(600);  
+                System.out.println("player got ID: " + playersID);
+                session.getUserProperties().put("playerID", playersID); //zapisuje polozenie  gracza
+                session.getUserProperties().put("xPos", newX); //zapisuje polozenie gracza
+                session.getUserProperties().put("yPos", newY); //zapisuje polozenie gracza    
                     
-                    session.getAsyncRemote().sendText(
-                                makeUserResponse(
-                                            Protocol.START.header, 
-                                            playersID + "", 
-                                            newX + "", 
-                                            newY + ""));
-                    
-                    players.add(session); 
-                    playersID += 1; 
-                      
-                    if(players.size() >= MAX_PLAYERS) {
-                        //jesli jest czterech przeciwnikow, kazdy dostaje info
-                        for (int i = 0; i < players.size(); i++){
-                            players.get(i).getAsyncRemote().sendText(Protocol.GAME.header);  
-                        }
-                        //session.getAsyncRemote().sendText(Protocol.GAME.header);
-                    }  
-                    break;                  
+                session.getAsyncRemote().sendText(
+                    makeUserResponse(
+                        Protocol.START.header,
+                        playersID + "",
+                        newX + "",
+                        newY + ""
+                    )
+                );
+                
+                players.add(session);
+                playersID += 1;
+                
+                if(players.size() >= MAX_PLAYERS) 
+                {
+                    //jesli jest czterech graczy, kazdy dostaje info o rozpoczęciu gry
+                    for (int i = 0; i < players.size(); i++)
+                    {
+                        players.get(i).getAsyncRemote().sendText(Protocol.GAME.header);
+                    }
+                }
+                break;  
+                
             case "1":
-               
-               players.get(Integer.parseInt(protocol[1])).getUserProperties().replace("xPos", protocol[2]);
-               players.get(Integer.parseInt(protocol[1])).getUserProperties().replace("yPos", protocol[3]);
-               
-               for (int i = 0; i < players.size(); i++){
+                players.get(Integer.parseInt(protocol[1])).getUserProperties().replace("xPos", protocol[2]);
+                players.get(Integer.parseInt(protocol[1])).getUserProperties().replace("yPos", protocol[3]);
+                
+                for (int i = 0; i < players.size(); i++)
+                {
                     players.get(i).getAsyncRemote().sendText(
-                            makeUserResponse(   Protocol.GAME.header, 
-                                                players.get(i).getUserProperties().get("playerID").toString(),
-                                                players.get(i).getUserProperties().get("xPos").toString(),
-                                                players.get(i).getUserProperties().get("yPos").toString()
-                            ) 
-                    ); 
-               } 
-   
+                        makeUserResponse(
+                            Protocol.GAME.header,
+                            players.get(i).getUserProperties().get("playerID").toString(),
+                            players.get(i).getUserProperties().get("xPos").toString(),
+                            players.get(i).getUserProperties().get("yPos").toString()
+                        )
+                    );
+                } 
+                break;
         }
-        System.out.println(csv);
     }
 
     @OnClose
-    public void onClose(Session session) { 
+    public void onClose(Session session)
+    { 
         SessionHandler.removeSession(session);
-        
-        //System.out.println("Session " + session.getId() + " has ended");
     }
     
-    private String makeUserResponse(String proto, String ID, String xpos, String ypos){
+    private String makeUserResponse(String proto, String ID, String xpos, String ypos)
+    {
         return proto + ":" + ID + ":" + xpos + ":" + ypos;
     }
 }
 
-enum Protocol {
+enum Protocol 
+{
     START("0"), GAME("1"), END("2");
     
     public String header;
     
-    Protocol(String protocolHeader) {
+    Protocol(String protocolHeader)
+    {
         header = protocolHeader;
     }
 }
